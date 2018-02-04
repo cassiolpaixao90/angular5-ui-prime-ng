ab96e2a2b8e99276e918e033545b2cf73b2332c8
Cássio Paixão <cassiopaixaoti@gmail.com>
2018-02-04 16:28:24 -0200

    adicionando autorizacao por role do usuario

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -12,7 +12,14 @@ import { LoginFormComponent } from './login-form/login-form.component';
 
 
 export function authHttpServiceFactory(http: Http, options: RequestOptions){
-  return new AuthHttp(new AuthConfig(), http, options);
+
+  const config= new AuthConfig({
+    globalHeaders:[
+      {'Content-Type': 'application/json'}
+    ]
+  });
+
+  return new AuthHttp(config, http, options);
 }
 
 @NgModule({
